18b572e5fbad2b32c4a7517f27f88c2c1890f2a1
tarunkishore <kishoretarunuem@gmail.com>
2026-02-26 08:59:27 +0530

    BookStore.java

--- src/test/java/com/qa/bookStore/BookStore.java
+++ src/test/java/com/qa/bookStore/BookStore.java
@@ -1,4 +1,22 @@
 package com.qa.bookStore;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.json.*;
+import org.testng.annotations.*;
+
 public class BookStore {
+    @Test
+    public void testGetBook(){
+        Response response = RestAssured
+           .given()
+                .baseUri("https://bookstore.toolsqa.com")
+           .when()
+                .get("/BookStore/v1/Books");
+        String res = response.getBody().asPrettyString();
+        System.out.println(res);
+        JSONObject rootJsonBody = new JSONObject(res);
+        JSONArray books = rootJsonBody.getJSONArray("books");
+
+    }
 }
