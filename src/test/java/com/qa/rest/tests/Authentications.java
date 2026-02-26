aa1af1f9c79c0e1488a85e502387423a60ec653b
tarunkishore <kishoretarunuem@gmail.com>
2026-02-26 09:02:23 +0530

    Authentication.java

--- src/test/java/com/qa/rest/tests/Authentications.java
+++ src/test/java/com/qa/rest/tests/Authentications.java
@@ -1,4 +1,19 @@
 package com.qa.rest.tests;
 
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class Authentications {
+
+    @Test
+    public void testBasicAuth(){
+        given()
+                .auth().preemptive().basic("postman", "password")
+                .when()
+                .get("https://postman-echo.com/basic-auth")
+                .then()
+                .statusCode(200)
+                .log().all();
+    }
 }
